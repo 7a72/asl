f8d3938c0ad63bd4dcf410d53404df4f06aa1220
c3bf26 <x@zrlab.org>
2024-12-13 09:09:47 +0800

    Add: Show Status in Module Manager

--- src/service.sh
+++ src/service.sh
@@ -6,7 +6,9 @@ MODDIR=${0%/*}
 
 if [ "${ASL_AUTO_START}" = enable ]; then
     printf "[%s]: asl init\n" "$(${ASL_BUSYBOX} date '+%Y-%m-%d %H:%M:%S')" > /data/local/asl/log/asl.log
+    sed -i "s/^description=.*/description=Android Subsystem for Linux (enable)/g" ${MODDIR}/module.prop
     ASL_QUIET=true /data/local/asl/asl_tools/asl_utils.sh -r "${ASL_FS_DIR}" -c "${ASL_INIT_CMD}"
 else
+    sed -i "s/^description=.*/description=Android Subsystem for Linux (disable)/g" ${MODDIR}/module.prop
     exit 0
 fi
